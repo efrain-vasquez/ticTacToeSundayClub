d7139fd141791a6784d90ee5b8d3778ad60efb06
thinkpad <xoalxiyetl@gmail.com>
2019-05-19 12:46:58 -0500

    working

---
+++ src/components/popUP.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class PopUP extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default PopUP;
